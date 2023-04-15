31dc675365293b8d0149395fa2e1d65a8c8c74b5
Ghostpiedpiper <2372958950@qq.com>
2023-04-15 14:40:46 +0800

    initail

---
+++ src/App.tsx
@@ -0,0 +1,107 @@
+import { useEffect, useRef, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { BufferAttribute, LinearEncoding, Mesh, PointLight, PointLightHelper, Vector2} from 'three'
+import { OrbitControls, PerspectiveCamera, Plane, Point, useHelper, useTexture } from '@react-three/drei'
+import { useControls } from "leva";
+
+
+function Terrain(){
+  const terrainTextures = useTexture({
+    map:'./textures/aerial_rocks_02_diff_4k.jpg',
+    displacementMap: './textures/aerial_rocks_02_disp_4k.jpg',
+    aoMap:'./textures/aerial_rocks_02_ao_4k.jpg',
+    roughnessMap:'./textures/aerial_rocks_02_rough_4k.jpg',
+    metalnessMap:'./textures/aerial_rocks_02_arm_4k_metalness.jpg',
+    normalMap:'./textures/aerial_rocks_02_nor_gl_4k.jpg',
+    alphaMap:'./textures/alpha.png',
+  });
+
+  const {displacementScale,aoMapIntensity,roughness,metalness,normalScale} = useControls({
+    displacementScale:{
+      value:1,
+      min:-2,
+      max:2,
+    },
+    aoMapIntensity:{
+      value:1,
+      min:0,
+      max:10,
+    },
+    roughness:{
+      value:1,
+      min:0,
+      max:1,
+    },
+    metalness:{
+      value:0,
+      min:0,
+      max:1,
+    },
+    normalScale:[1,1],
+  });
+
+  const mesh = useRef<Mesh>(null!);
+  useEffect(() => {
+    mesh.current.geometry.setAttribute(
+      "uv2",
+      new BufferAttribute(mesh.current.geometry.attributes.uv.array, 2)
+    );
+  });
+  
+  return(
+    <Plane args={[10,10,128,128]} rotation-x={-Math.PI / 2} ref={mesh}>
+      <meshStandardMaterial
+      {...terrainTextures}
+      normalMap-encoding={(LinearEncoding)}
+      displacementScale = {displacementScale}
+      aoMapIntensity={aoMapIntensity}
+      roughness={roughness}
+      metalness={metalness}
+      metalnessMap={null}
+      normalScale={new Vector2(normalScale[0],normalScale[1])}
+      transparent
+      />
+    </Plane>
+    );
+}
+
+function ThreeContent(){
+
+  const lightRef = useRef<PointLight>(null!);
+  useHelper(lightRef,PointLightHelper,1,"red");
+  return(
+    <>
+      <ambientLight />
+      <pointLight ref={lightRef} position={[5,5,0]} intensity={2} />
+      <OrbitControls />
+      <Terrain />
+    </>
+    );
+}
+
+  
+
+function ThreeScene(){
+
+  return(
+    <Canvas id='Canvas' camera={{position:[0,10,5]}} >
+    <ThreeContent />
+    </Canvas>
+
+    );
+}
+
+
+function App() {
+
+  return (
+    <div className='App h-screen'>
+      <ThreeScene />
+    </div>
+  );
+}
+
+export default App
